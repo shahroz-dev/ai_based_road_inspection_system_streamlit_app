1b4c7352f7805329c030de69465c4251de39a85b
Muhammad Shahroz <shahrozarif28@gmail.com>
2022-11-13 00:43:42 +0500

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2fddc8cfdbec37bea522ba5bd031e319b11a3501bfb1685dc3a64557a8c5438e
+size 100
---
+++ main.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6cfbe70e8d41ccf5a3868a0c3481870b62c25f86cdecee631de9b3567ff50273
+size 6508
